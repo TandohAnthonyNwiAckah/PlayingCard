0b040ad8d84420691e24ef6ce74f5728e70be867
Tandoh <anthony.tandoh@tanacom.io>
2020-06-13 20:52:43 +0000

    commit UIBezierPath()

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -17,23 +17,36 @@ class PlayingCardView: UIView {
     override func draw(_ rect: CGRect) {
         // Drawing code
         
-        if let context = UIGraphicsGetCurrentContext(){
-            
-            context.addArc(center: CGPoint(x:bounds.midX,y:bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-            
-            context.setLineWidth(5.0)
-            
-            UIColor.green.setFill()
-            
-            UIColor.red.setStroke()
-            
-            context.strokePath()
-            
-            context.fillPath()
-            
-        }
+//        if let context = UIGraphicsGetCurrentContext(){
+//
+//            context.addArc(center: CGPoint(x:bounds.midX,y:bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//
+//            context.setLineWidth(5.0)
+//
+//            UIColor.green.setFill()
+//
+//            UIColor.red.setStroke()
+//
+//            context.strokePath()
+//
+//            context.fillPath()
+//
+//        }
         
         
+        let path = UIBezierPath()
+        
+        path.addArc(withCenter: CGPoint(x:bounds.midX,y:bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+                 
+        path.lineWidth = 5.0
+        
+        UIColor.green.setFill()
+                 
+        UIColor.red.setStroke()
+        
+        path.stroke()
+        
+        path.fill()
         
         
     }
