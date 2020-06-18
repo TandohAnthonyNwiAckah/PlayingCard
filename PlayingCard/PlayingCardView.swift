d2ec80e75e4cd2d6b0495580ba5f8f2d6ae35fd7
Tandoh <anthony.tandoh@tanacom.io>
2020-06-18 21:08:51 +0000

    commit  UISwipeGestureRecognizer on Card

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 
+/**NB  @IBDesignable provides functionality for live rendering of changes of our custom views directly in a Storyboard or .xib.
+ *All we have to do is mark the class of a custom view with a @IBDesignable attribute and implement the prepareForInterfaceBuilder() method.
+ * NB @IBInspectable allows us to create attributes in code that we can assign in a storyboard or a .xib file.
+ * For example, when we want a cornerRadius *property available in a Storyboard, we create a cornerRadius property inside our custom view
+ * and mark it with @IBInspectable.
+ */
+
+
 @IBDesignable
 class PlayingCardView: UIView {
     
@@ -157,8 +165,6 @@ class PlayingCardView: UIView {
     override func draw(_ rect: CGRect) {
         // Drawing code
         
-        
-    
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         
         roundedRect.addClip()
--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -10,17 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+
     var deck = CardDeck()
+    
+    
+    @IBOutlet weak var playingCardView: PlayingCardView!{
+        
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            
+            swipe.direction = [.left, .right]
+            
+            playingCardView.addGestureRecognizer(swipe)
+            
+    
+        }
+        
+    }
+    
+    
+    
+    
+    @objc func nextCard() {
+        if let card  = deck.draw() {
+            playingCardView.rank = card.rank.order()
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        for _ in 1...10{
-           if let card = deck.draw(){
-                print("\(card)")
-            }
-        }    
+        
+        
+//        for _ in 1...10{
+//           if let card = deck.draw(){
+//                print("\(card)")
+//            }
+//        }
+        
+        
+        
+        
+        
     }
+    
+    
+    
+    
+    
+    
 
 }
 
